73da20db1ce2cc5366f50c9ba085cb2fb4895ca6
KelvinJPS <kelvinjps@pop-os.localdomain>
2023-01-27 03:14:30 -0500

    lab8triviacompleted

---
+++ week_8/lab8/trivia/script.js
@@ -0,0 +1,32 @@
+// Change the color of a button clicked base  on whether or not the button containts the correct answer
+function checkAnswer(user_answer) {
+	const correctAnswers = ["Python","TCP"];
+	console.log(user_answer)
+	return correctAnswers.includes(user_answer);
+
+}
+function answerButtonColor(button){
+
+	if (checkAnswer(button.id)) {
+		button.style.backgroundColor = 'green';
+	}
+	else {
+		button.style.backgroundColor = 'red';
+	}
+}
+function multipleChoiceResult() {
+	let choiceButtons = document.querySelectorAll('.multiple_choice')
+	choiceButtons.forEach(function (button) {
+		button.onclick = () => answerButtonColor(button);
+	})
+}
+document.addEventListener('DOMContentLoaded', function () {
+	multipleChoiceResult();
+	document.querySelector('form').addEventListener('submit', function (event) {
+		let userAnswer = document.querySelector('#user_answer').value;
+		alert("answer is:" + checkAnswer(userAnswer));
+		event.preventDefault();
+	})
+
+
+})
