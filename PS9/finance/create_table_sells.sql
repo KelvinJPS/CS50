6120b6f5ef14e97ad9414915ab431ead22869991
KelvinJPS <kelvinjps@pop-os.localdomain>
2023-02-06 15:01:22 -0500

    change userid type from text to integer in creation query

--- PS9/finance/create_table_sells.sql
+++ PS9/finance/create_table_sells.sql
@@ -1,10 +1,10 @@
 CREATE TABLE sells( 
     sellid     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    userid     TEXT NOT NULL, 
+    userid     INT NOT NULL, 
     date       TEXT NOT NULL,
     symbol     TEXT NOT NULL,
     shares     INTEGER NOT NULL, 
     price      INTEGER NOT NULL,
     FOREIGN KEY(userid) REFERENCES users(id)
 );
-CREATE UNIQUE INDEX sellid ON purchases (sellid);
+CREATE UNIQUE INDEX sellid ON sells (sellid);
